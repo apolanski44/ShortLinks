5d0c1e8263fe1bf6220e428d6f7382b9c8d84464
Adrian <apolanski1234@gmail.com>
2025-02-08 16:59:18 +0100

    Dodano pozostale pliki

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ShortLinksApp</title>
+        @vite(['resources/js/app.js','resources/css/app.css'])
+    </head>
+    <body>
+        <div id="app">
+            <example-component></example-component>
+        </div>
